c61ad7221b65a2eb4a952113882c2b9add2af634
amarey <amanda.p.reyes@gmail.com>
2015-12-17 13:38:00 +0100

    Fixed merge

---
+++ markup/script/script.js
@@ -0,0 +1,8 @@
+$(document).ready(function() { 
+	$('.submenu-icon').on('click', function(){
+		//$(this).addClass('hej');
+		$('.thirdmenu').toggle();
+	});
+$(".toggle-menu").click(function () {	
+	$(".menu").slideToggle(200);
+});
