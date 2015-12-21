ce7e639b4e34354d108dbdf914161f17a3815786
amarey <amanda.p.reyes@gmail.com>
2015-12-21 15:29:08 +0100

    collapse style and script

--- markup/script/script.js
+++ markup/script/script.js
@@ -9,6 +9,22 @@ $(".toggle-menu").click(function () {
 	$(".menu").slideToggle(200);
 });
 
+$(".show-all").keyup(function(event){
+    if(event.keyCode === 13){
+        $(".show-all").click();
+    }
+});
+
 $(".show-all").click(function () {	
 	$(".all").slideToggle(200);
+	$(".arrow").toggleClass('collapse');
+	var el = $('.text-div');
+	if (el.text() === el.data("text")) {
+	  el.text(el.data("text-original"));
+	} else {
+	  el.data("text-original", el.text());
+	  el.text(el.data("text"));
+	}
 });
+
+
