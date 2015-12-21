af2ab5e1d5fb402f07fc2519591551673c5554fd
matildaaspman <matasp@Matildas-MacBook-Pro-2.local>
2015-12-21 16:47:49 +0100

    Vänsternavigering

--- markup/script/script.js
+++ markup/script/script.js
@@ -1,30 +1,35 @@
 $(document).ready(function() { 
+
+	//Toggle leftnavigation and icon
 	$('.expand').on('click', function(){
-		$(this).closest('li').find('.thirdmenu').toggle();
+		$(this).closest('li').find('.js-toggle:first').toggle();
 		$(this).toggleClass('open');
 	});
-});
 
-$(".toggle-menu").click(function () {	
-	$(".menu").slideToggle(200);
-});
+	// Toggle mobilemenu
+	$(".toggle-menu").click(function () {	
+		$(".menu").slideToggle(200);
+	});
 
-$(".show-all").keyup(function(event){
-    if(event.keyCode === 13){
-        $(".show-all").click();
-    }
-});
+	// Show all button
+	$(".show-all").keyup(function(event){
+	    if(event.keyCode === 13){
+	        $(".show-all").click();
+	    }
+	});
 
-$(".show-all").click(function () {	
-	$(".all").slideToggle(200);
-	$(".arrow").toggleClass('collapse');
-	var el = $('.text-div');
-	if (el.text() === el.data("text")) {
-	  el.text(el.data("text-original"));
-	} else {
-	  el.data("text-original", el.text());
-	  el.text(el.data("text"));
-	}
+	$(".show-all").click(function () {	
+		$(".all").slideToggle(200);
+		$(".arrow").toggleClass('collapse');
+		var el = $('.text-div');
+		if (el.text() === el.data("text")) {
+		  el.text(el.data("text-original"));
+		} else {
+		  el.data("text-original", el.text());
+		  el.text(el.data("text"));
+		}
+	});
+
 });
 
 
